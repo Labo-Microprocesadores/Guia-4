9a5450e7931219fd99a8d0c22bbba19925b858a5
scheinpablo <scheinpablo@yahoo.com>
2020-09-26 00:36:38 -0300

    Update App.c
    test with 2 callbacks

--- MultipleSystickCallbacks/source/App.c
+++ MultipleSystickCallbacks/source/App.c
@@ -24,7 +24,7 @@
 
 static void delayLoop(uint32_t veces);
 void callback(void);
-
+void callback2(void);
 
 static uint32_t b_counter = DELAY;
 /*******************************************************************************
@@ -39,11 +39,14 @@ void App_Init (void)
     gpioMode(SW, INPUT_PULLUP);
     gpioMode(LED, OUTPUT);
     gpioMode(PIN_LED_RED, OUTPUT);
+    gpioMode(PIN_LED_GREEN, OUTPUT);
 
     //gpioWrite(LED, HIGH);
-    gpioWrite(PIN_LED_RED, HIGH);
+    gpioWrite(PIN_LED_GREEN, LOW);
+    gpioWrite(PIN_LED_RED, LOW);
     SysTick_Init();
     SysTick_AddCallback(&callback, 12500000L*8); //1s
+    SysTick_AddCallback(&callback2, 12500000L*4); //0.5s
 }
 
 /* Función que se llama constantemente en un ciclo infinito */
@@ -71,14 +74,32 @@ void callback(void)
 	i++;
 	gpioToggle(PIN_LED_RED);
 
-	if (i == 5){
+	if (i == 10){
 		Systick_ChangeCallbackTime(&callback, 12500000L*4);	//Makes it faster after 5 cycles.
 
 	}
 
-	if (i == 15){
+	if (i == 30){
 
 		Systick_ClrCallback(&callback);	//Cancels the toggle after 15 cycles.
+	}
+
+}
+void callback2(void)
+{
+	static int i = 0;
+
+	i++;
+	gpioToggle(PIN_LED_GREEN);
+
+	if (i == 10){
+		Systick_ChangeCallbackTime(&callback2, 12500000L*2);	//Makes it faster after 5 cycles.
+
+	}
+
+	if (i == 30){
+
+		Systick_ClrCallback(&callback2);	//Cancels the toggle after 15 cycles.
 	}
 
 }
