21c481890fbfc3a61e4f7ad9c1ac10f22f64b2a4
malenamuller <mmuller@itba.edu.ar>
2020-10-04 22:02:09 -0300

    7SegDisplay

--- 7SegDisplay/Debug/objects.mk
+++ 7SegDisplay/Debug/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := C:/Users/malem/Documents/MCUXpressoIDE_10.2.0_759/workspace/7SegDisplay/source/gpio.o
+USER_OBJS := C:/Users/malem/Desktop/Guia4/7SegDisplay/source/gpio.o
 
 LIBS :=
 
