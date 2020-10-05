d566d122d204c2ae0fd729d9256dc69bd9fa5aa8
Matias Francois <mfrancois@itba.edu.ar>
2020-10-05 00:26:57 -0300

    comments

--- 7SegDisplay/source/7SegDisplay.c
+++ 7SegDisplay/source/7SegDisplay.c
@@ -8,16 +8,23 @@
 #include "SysTick.h"
 #include "gpio.h"
 
-
-bool printCaracter(uint8_t caracter, pin_t pins[])
+/**
+ * @brief print one character on one 7 segments display
+ * @param character to print
+ * @param array with pins to 7 segments display
+ * @return printed succeed
+ */
+bool printCaracter(uint8_t character, pin_t pins[])
 {
 	bool result[8];
 	int count;
+	//convert the number to binary and then with shifts find each bit, put them in result array
 	for(count=0; count<8; count++)
 	{
-		result[count]=(bool)(caracter & controller);
-		caracter = caracter>>1;
+		result[count]=(bool)(character & controller);
+		character = character>>1;
 	}
+	//turn on the pins acording to result array
 	for(count=0; count<8; count++)
 	{
 		gpioWrite(pins[count], result[count]);
